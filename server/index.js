604fd221d7c1e7217c5bb8add529729431b10f4b
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2022-03-09 17:16:02 -0800

    dotenv dep install, mongo user/pass hidden

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@
 const express = require('express');
 // variable to pull from express
 const app = express();
+// introduces the dotenv
+require('dotenv').config();
+const dbUser = process.env.MONGO_USER
+const dbPass = process.env.MONGO_PASS
 
 const mongoose = require('mongoose');
 
-mongoose.connect()
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.6ajsr.mongodb.net/merntutorial?retryWrites=true&w=majority`)
 
 // get api to 'start', call back function will run is all goes well
 app.listen(3001, () => {
