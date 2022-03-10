fde43b73be7861df41dab5882b2168d28fb892fd
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2022-03-09 17:43:51 -0800

    post request made and successful

--- server/index.js
+++ server/index.js
@@ -13,8 +13,12 @@ const dbPass = process.env.MONGO_PASS;
 
 const mongoose = require('mongoose');
 
+// needed to parse through body requests
+app.use(express.json());
+
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.6ajsr.mongodb.net/merntutorial?retryWrites=true&w=majority`)
 
+// GET request
 app.get("/getUsers", (req, res)=> {
     UserModel.find({}, (err, result) => {
         if (err) {
@@ -24,6 +28,17 @@ app.get("/getUsers", (req, res)=> {
         }
     });
 });
+
+// POST request
+app.post("/createUser", async (req, res) => {
+    // represents the data we want to insert in our db coming from front end
+    const user = req.body
+    // use the imported model to create a new user with the contents of the req.body
+    const newUser = new UserModel(user);
+    await newUser.save();
+    // send back our new user
+    res.json(user)
+})
 
 // get api to 'start', call back function will run is all goes well
 app.listen(3001, () => {
